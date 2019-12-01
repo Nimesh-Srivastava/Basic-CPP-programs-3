87563d627e584438231b8dd8ac6400229afa99aa
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-12-02 00:12:26 +0530

    Create L3.cpp

---
+++ L3.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void main()
+{
+	system("cls");
+
+	/*	int a, b;
+	cout << "Enter a number : ";
+	cin >> a;
+	cout << "Enter another number : ";
+	cin >> b;
+	(a < b) ? cout << b << " is larger" : cout << a << " is larger";
+	cout << endl;*/
+
+	/*int a, b, c;
+	cout << "Enter a number : ";
+	cin >> a;
+	cout << "Enter another number : ";
+	cin >> b;
+	cout << "Enter yet another number : ";
+	cin >> c;
+	(a > b) ? (b > c) ? cout << a << " is largest" : (c > a) ? cout << c << " is the largest" : cout << b << " is the largest" : cout << endl;
+	cout << endl;*/
+
+
+	/*int a;
+	cout << "Enter a number : ";
+	cin >> a;
+	(a % 2 == 0) ? cout << "Number is even" : cout << "Number is odd" << endl;*/
+
+
+	/*int year;
+	cout << "Enter the year : ";
+	cin >> year;
+	(year % 4 == 0) ? cout << "Year is leap" : cout << "Year is not leap" << endl;*/
+
+	/*int num;
+	cout << "Enter weekday number : ";
+	cin >> num;
+	cout << "Day is ";
+	switch (num)
+	{
+	case 1:
+		cout << "Monday" << endl;
+		break;
+	case 2:
+		cout << "Tuesday" << endl;
+		break;
+	case 3:
+		cout << "Wednesday" << endl;
+		break;
+	case 4:
+		cout << "Thursday" << endl;
+		break;
+	case 5:
+		cout << "Friday" << endl;
+		break;
+	case 6:
+		cout << "Saturday" << endl;
+		break;
+	case 7:
+		cout << "Sunday" << endl;
+		break;
+	default:
+		cout << "Wrong input" << endl;
+	}*/
+
+	/*int a, b, opt;
+	cout << "Enter a : ";
+	cin >> a;
+	cout << "Enter b : ";
+	cin >> b;
+	cout << "\nEnter the option that you want to perform :- " << endl;
+	cout << "1) Addition\t2) Subtraction" << endl;
+	cout << "3) Multiplication\t4) Division" << endl;
+	cout << "Enter the option : ";
+	cin >> opt;
+	switch (opt)
+	{
+	case 1:
+		cout << "The sum is : " << a + b;
+		break;
+	case 2:
+		cout << "a - b is : " << a - b << endl;
+		cout << "b - a is : " << b - a;
+		break;
+	case 3:
+		cout << "a * b is : " << a * b;
+		break;
+	case 4:
+		cout << "a / b is : " << a / b << endl;
+		cout << "b / a is : " << b / a;
+		break;
+	default:
+		cout << "Wrong input";
+	}
+	cout << endl;*/
+
+	/*char a;
+	int b;
+	cout << "Enter a character : ";
+	cin >> a;
+	a = toupper(a);
+	b = a;
+	switch (b) {
+	case 65:
+		cout << "That is a vowel";
+		break;
+	case 69:
+		cout << "That is a vowel";
+		break;
+	case 73:
+		cout << "That is a vowel";
+		break;
+	case 79:
+		cout << "That is a vowel";
+		break;
+	case 85:
+		cout << "That is a vowel";
+		break;
+	default:
+		cout << "That is a consonant";
+	}
+	cout << endl;*/
+
+	/*int i = 5, j = 6, k = 7;
+	if (i > j == k)
+		cout << i++ << " " << ++j << " " << --k << endl;
+	else
+		cout << i << " " << j << " " << k << endl;
+	*/
+
+	(void)_getch();
+}
